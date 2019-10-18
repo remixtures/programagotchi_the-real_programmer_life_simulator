e6ac2fe2ae86c13e1df145766131fa8d65b9bf7a
Loispealz <rune.fixe@gmail.com>
2019-10-18 17:43:10 +0100

    GFX tests basically comcluded. a

--- programmerLifeSimulator/src/org/academiadecodigo/stringteasers/programmators/game/graphic/testGFX.java
+++ programmerLifeSimulator/src/org/academiadecodigo/stringteasers/programmators/game/graphic/testGFX.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.stringteasers.programmators.game.graphic;
 
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Text;
 
 public class testGFX  {
 
@@ -42,18 +43,36 @@ public class testGFX  {
 
         Rectangle action1 =new Rectangle(57, 550, 40, 40);
         action1.draw();
+        Text text1 = new Text(74,565, "H");
+        text1.grow(10,10);
+        text1.draw();
+
 
         Rectangle action2 =new Rectangle(137, 550, 40, 40);
         action2.draw();
+        Text text2 = new Text(156,565, "F");
+        text2.grow(10,10);
+        text2.draw();
 
         Rectangle action3 =new Rectangle(217, 550, 40, 40);
         action3.draw();
+        Text text3 = new Text(237,565, "S");
+        text3.grow(10,10);
+        text3.draw();
 
         Rectangle action4 =new Rectangle(297, 550, 40, 40);
         action4.draw();
+        Text text4 = new Text(317,565, "W");
+        text4.grow(10,10);
+        text4.draw();
+
 
         Rectangle textBox = new Rectangle(40,200, 315, 340 );
         textBox.draw();
+
+        Text TextHealth = new Text(70, 220, "1. " + " Go to the Doctor.");
+        TextHealth.grow(10, 7);
+        TextHealth.draw();
 
 
         Thread.sleep(1000);
